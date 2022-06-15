33a89dc82dc2508bc3b8e4e0cacf02d6631d8530
Theodore NATON <tnaton@e3r12p14.clusters.42paris.fr>
2022-06-15 20:19:48 +0200

    bite

--- string.c
+++ string.c
@@ -6,20 +6,81 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:40:54 by tnaton            #+#    #+#             */
-/*   Updated: 2022/06/14 20:27:09 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/06/15 20:18:59 by tnaton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+char	*addspace(char *ret, int size, int type)
+{
+	char	*space;
+
+	space = malloc(sizeof(char) * size);
+	space[size] = '\0';
+	while (size--)
+		space[size] = ' ';
+	if (type)
+		return (ft_strjoin_free(ret, space));
+	return (ft_strjoin_free(space, ret));
+}
+
+void	flag(t_str *id, char *ret)
+{
+	int	i;
+	int	last;
+
+	i = 1;
+	while (id->str[i])
+	{
+
+	}
+	while (id->str[i])
+	{
+		if (isdigit(id->str[i]))
+		{
+			last = i;
+			while (ft_isdigit(id->str[i]))
+				i++;
+			last = ft_atoi_free(ft_substr(id->str, last, i - last));
+			if (last < (int)ft_strlen(ret))
+			{
+				free(id->str);
+				id->str = ret;
+				return ;
+			}
+			ret = addspace(ret, (last - ft_strlen(ret)), 0);
+		}
+		if (id->str[i] == '-')
+		{
+			while (id->str[i] == '-')
+				i++;
+			last = i;
+			while (ft_isdigit(id->str[i]))
+				i++;
+			last = ft_atoi_free(ft_substr(id->str, last, i - last));
+			if (last < (int)ft_strlen(ret))
+			{
+				free(id->str);
+				id->str = ret;
+				return ;
+			}
+			ret = addspace(ret, (last - ft_strlen(ret)), 1);
+		}
+		i++;
+	}
+	free(id->str);
+	id->str = ret;
+}
 
 void	types(t_str *current, va_list *arg)
 {
 	char	*ret;
 
 	ret = (char *)va_arg(*arg, char *);
-	free(current->str);
 	if (ret)
-		current->str = ft_strdup(ret);
+		ret = ft_strdup(ret);
 	else
-		current->str = ft_strdup("(null)");
+		ret = ft_strdup("(null)");
+	flag(current, ret);
 }
