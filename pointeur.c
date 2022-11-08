f0c3bfcfebe081d30c86fabb51b9a4f292d55398
Theodore NATON <tnaton@e3r11p12.clusters.42paris.fr>
2022-11-08 18:37:58 +0100

    ca fonctionne bieng

--- pointeur.c
+++ pointeur.c
@@ -6,49 +6,66 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:41:17 by tnaton            #+#    #+#             */
-/*   Updated: 2022/06/16 12:52:36 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/11/08 18:36:41 by tnaton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+int	pointflag2(t_str *id, char **ret, int *i)
+{
+	int	last;
+
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last < (int)ft_strlen(*ret))
+	{
+		free(id->str);
+		id->str = *ret;
+		return (1);
+	}
+	*ret = addspace(*ret, (last - ft_strlen(*ret)), 0);
+	return (0);
+}
+
+int	pointflag3(t_str *id, char **ret, int *i)
+{
+	int	last;
+
+	while (id->str[*i] == '-')
+		(*i)++;
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last < (int)ft_strlen(*ret))
+	{
+		free(id->str);
+		id->str = *ret;
+		return (1);
+	}
+	*ret = addspace(*ret, (last - ft_strlen(*ret)), 1);
+	return (0);
+}
+
 void	pointflag(t_str *id, char *ret)
 {
 	int	i;
-	int	last;
 
 	i = 1;
 	while (id->str[i])
 	{
 		if (isdigit(id->str[i]) && id->str[i - 1] != '.')
 		{
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < (int)ft_strlen(ret))
-			{
-				free(id->str);
-				id->str = ret;
+			if (pointflag2(id, &ret, &i))
 				return ;
-			}
-			ret = addspace(ret, (last - ft_strlen(ret)), 0);
 		}
 		if (id->str[i] == '-')
 		{
-			while (id->str[i] == '-')
-				i++;
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < (int)ft_strlen(ret))
-			{
-				free(id->str);
-				id->str = ret;
+			if (pointflag3(id, &ret, &i))
 				return ;
-			}
-			ret = addspace(ret, (last - ft_strlen(ret)), 1);
 		}
 		i++;
 	}
