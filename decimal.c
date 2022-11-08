3f111a812d78139faf38a928e9d418b440d78500
Theodore NATON <tnaton@e3r11p12.clusters.42paris.fr>
2022-11-08 17:55:14 +0100

    ca marche rn

--- decimal.c
+++ decimal.c
@@ -6,7 +6,7 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:41:46 by tnaton            #+#    #+#             */
-/*   Updated: 2022/11/08 17:24:38 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/11/08 17:32:02 by tnaton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,43 +109,60 @@ char	*intflag2(t_str *id, char *ret)
 	return (ret);
 }
 
+int	intflag31(t_str *id, char ***ret, int *i)
+{
+	int	last;
+
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last < (int)ft_strlen(**ret))
+	{
+		free(id->str);
+		id->str = **ret;
+		return (1);
+	}
+	**ret = addspace(**ret, (last - ft_strlen(**ret)), 0);
+	return (0);
+}
+
+int	intflag32(t_str *id, char ***ret, int *i)
+{
+	int	last;
+
+	while (id->str[*i] == '-')
+		(*i)++;
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last < (int)ft_strlen(**ret))
+	{
+		free(id->str);
+		id->str = **ret;
+		return (1);
+	}
+	**ret = addspace(**ret, (last - ft_strlen(**ret)), 1);
+	return (0);
+}
+
 int	intflag3(t_str *id, char **ret)
 {
 	int	i;
-	int	last;
 
 	i = 1;
 	while (id->str[i])
 	{
 		if (isdigit(id->str[i]) && id->str[i - 1] != '.')
 		{
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < (int)ft_strlen(*ret))
-			{
-				free(id->str);
-				id->str = *ret;
+			if (intflag31(id, &ret, &i))
 				return (1);
-			}
-			*ret = addspace(*ret, (last - ft_strlen(*ret)), 0);
 		}
 		if (id->str[i] == '-')
 		{
-			while (id->str[i] == '-')
-				i++;
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < (int)ft_strlen(*ret))
-			{
-				free(id->str);
-				id->str = *ret;
+			if (intflag32(id, &ret, &i))
 				return (1);
-			}
-			*ret = addspace(*ret, (last - ft_strlen(*ret)), 1);
 		}
 		i++;
 	}
--- hexa.c
+++ hexa.c
@@ -6,90 +6,134 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:43:03 by tnaton            #+#    #+#             */
-/*   Updated: 2022/06/16 17:02:33 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/11/08 17:54:26 by tnaton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	hexaflag(t_str *id, char *ret, char *exa)
+char	*hexaflag21(t_str *id, char *ret, int *i)
 {
-	int	i;
 	int	last;
 	int	isneg;
+
+	ret = getneg(ret, &isneg);
+	while (id->str[*i] == '0')
+		(*i)++;
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last > (int)ft_strlen(ret))
+		ret = addzero(ret, last - ft_strlen(ret) - isneg, 0);
+	if (isneg)
+		ret = ft_strjoin_free(ft_strdup("-"), ret);
+	return (ret);
+}
+
+char	*hexaflag22(t_str *id, char *ret, int *i)
+{
+	int	last;
+	int	isneg;
+
+	ret = getneg(ret, &isneg);
+	(*i)++;
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last > (int)ft_strlen(ret))
+		ret = addzero(ret, last - ft_strlen(ret), 0);
+	else if (ft_strlen(ret) == 1 && ret[0] == '0' && last < 1)
+	{
+		free(ret);
+		ret = ft_strdup("");
+	}
+	if (isneg)
+		ret = ft_strjoin_free(ft_strdup("-"), ret);
+	return (ret);
+}
+
+char	*hexaflag2(t_str *id, char *ret)
+{
+	int	i;
 
 	i = 1;
 	while (id->str[i])
 	{
 		if (i == 1 && id->str[i] == '0' && !aschr(id->str, '.'))
-		{
-			ret = getneg(ret, &isneg);
-			while (id->str[i] == '0')
-				i++;
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last > (int)ft_strlen(ret))
-				ret = addzero(ret, last - ft_strlen(ret) - isneg, 0);
-			if (isneg)
-				ret = ft_strjoin_free(ft_strdup("-"), ret);
-		}
+			ret = hexaflag21(id, ret, &i);
 		if (id->str[i] == '.')
-		{
-			ret = getneg(ret, &isneg);
-			i++;
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last > (int)ft_strlen(ret))
-				ret = addzero(ret, last - ft_strlen(ret), 0);
-			else if (ft_strlen(ret) == 1 && ret[0] == '0' && last < 1)
-			{
-				free(ret);
-				ret = ft_strdup("");
-			}
-			if (isneg)
-				ret = ft_strjoin_free(ft_strdup("-"), ret);
-		}
+			ret = hexaflag22(id, ret, &i);
 		i++;
 	}
+	return (ret);
+}
+
+int	hexaflag31(t_str *id, char ***ret, int *i)
+{
+	int	last;
+
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last < (int)ft_strlen(**ret))
+	{
+		free(id->str);
+		id->str = **ret;
+		return (1);
+	}
+	**ret = addspace(**ret, (last - ft_strlen(**ret)), 0);
+	return (0);
+}
+
+int	hexaflag32(t_str *id, char ***ret, int *i)
+{
+	int	last;
+
+	while (id->str[*i] == '-')
+		(*i)++;
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last < (int)ft_strlen(**ret))
+	{
+		free(id->str);
+		id->str = **ret;
+		return (1);
+	}
+	**ret = addspace(**ret, (last - ft_strlen(**ret)), 1);
+	return (0);
+}
+
+int	hexaflag3(t_str *id, char **ret)
+{
+	int	i;
+
 	i = 1;
 	while (id->str[i])
 	{
 		if (isdigit(id->str[i]) && id->str[i - 1] != '.')
 		{
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < (int)ft_strlen(ret))
-			{
-				free(id->str);
-				id->str = ret;
-				return ;
-			}
-			ret = addspace(ret, (last - ft_strlen(ret)), 0);
+			if (hexaflag31(id, &ret, &i))
+				return (1);
 		}
 		if (id->str[i] == '-')
 		{
-			while (id->str[i] == '-')
-				i++;
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < (int)ft_strlen(ret))
-			{
-				free(id->str);
-				id->str = ret;
-				return ;
-			}
-			ret = addspace(ret, (last - ft_strlen(ret)), 1);
+			if (hexaflag32(id, &ret, &i))
+				return (1);
 		}
 		i++;
 	}
+	return (0);
+}
+
+char	*hexaflag4(t_str *id, char *ret)
+{
+	int	isneg;
+
 	if (aschr(id->str, ' '))
 	{
 		ret = getneg(ret, &isneg);
@@ -106,11 +150,18 @@ void	hexaflag(t_str *id, char *ret, char *exa)
 		else
 			ret = ft_strjoin_free(ft_strdup("-"), ret);
 	}
+	return (ret);
+}
+
+void	hexaflag(t_str *id, char *ret, char *exa)
+{
+	ret = hexaflag2(id, ret);
+	if (hexaflag3(id, &ret))
+		return ;
+	ret = hexaflag4(id, ret);
 	if (aschr(id->str, '#'))
 	{
-		if (ft_strlen(ret) == 1 && ret[0] == '0')
-			isneg = 0;
-		else
+		if (!(ft_strlen(ret) == 1) || !(ret[0] == '0'))
 			ret = ft_strjoin_free(ft_strdup(exa), ret);
 	}
 	free(id->str);
