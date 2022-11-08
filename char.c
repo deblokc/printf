5c5d270e4f2f260cc92920e84511ec8f16003637
Theodore NATON <tnaton@e3r11p12.clusters.42paris.fr>
2022-11-08 17:25:48 +0100

    pour le moment ca marche

--- char.c
+++ char.c
@@ -6,7 +6,7 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:40:07 by tnaton            #+#    #+#             */
-/*   Updated: 2022/06/16 17:14:34 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/11/08 16:25:10 by tnaton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,40 +21,47 @@ void	changeid(t_str *id, int padding)
 		id->str[padding] = ' ';
 }
 
+int	charflag2(t_str *id, int i, int last)
+{
+	last = i;
+	while (ft_isdigit(id->str[i]))
+		i++;
+	last = ft_atoi_free(ft_substr(id->str, last, i - last));
+	if (last < 2)
+		return (0);
+	changeid(id, last);
+	id->type = 2;
+	return (1);
+}
+
+int	charflag3(t_str *id, int i, int last)
+{
+	while (id->str[i] == '-')
+		i++;
+	last = i;
+	while (ft_isdigit(id->str[i]))
+		i++;
+	last = ft_atoi_free(ft_substr(id->str, last, i - last));
+	if (last < 2)
+		return (0);
+	changeid(id, last);
+	id->type = 3;
+	return (1);
+}
+
 int	charflag(t_str *id)
 {
 	int		last;
 	int		i;
 
 	i = 1;
+	last = 0;
 	while (id->str[i])
 	{
 		if (ft_isdigit(id->str[i]))
-		{
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < 2)
-				return (0);
-			changeid(id, last);
-			id->type = 2;
-			return (1);
-		}
+			return (charflag2(id, i, last));
 		else if (id->str[i] == '-')
-		{
-			while (id->str[i] == '-')
-				i++;
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < 2)
-				return (0);
-			changeid(id, last);
-			id->type = 3;
-			return (1);
-		}
+			return (charflag3(id, i, last));
 		i++;
 	}
 	return (0);
--- decimal.c
+++ decimal.c
@@ -6,7 +6,7 @@
 /*   By: tnaton <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:41:46 by tnaton            #+#    #+#             */
-/*   Updated: 2022/11/08 13:21:14 by tnaton           ###   ########.fr       */
+/*   Updated: 2022/11/08 17:24:38 by tnaton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,81 +51,68 @@ int	aschr(char *str, char c)
 	return (0);
 }
 
-void	intflag21(t_str *id, char *ret, int *i, int *last, int *isneg)
+char	*intflag21(t_str *id, char *ret, int *i)
 {
-	if (id->str[*i] == '.')
-	{
-		ret = getneg(ret, isneg);
-		*i++;
-		*last = *i;
-		while (ft_isdigit(id->str[*i]))
-			*i++;
-		*last = ft_atoi_free(ft_substr(id->str, *last, *i - *last));
-		if (*last > (int)ft_strlen(ret))
-			ret = addzero(ret, *last - ft_strlen(ret), 0);
-		else if (ft_strlen(ret) == 1 && ret[0] == '0' && *last < 1)
-		{
-			free(ret);
-			ret = ft_strdup("");
-		}
-		if (*isneg)
-			ret = ft_strjoin_free(ft_strdup("-"), ret);
-	}
+	int	last;
+	int	isneg;
+
+	ret = getneg(ret, &isneg);
+	while (id->str[*i] == '0')
+		(*i)++;
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last > (int)ft_strlen(ret))
+		ret = addzero(ret, last - ft_strlen(ret) - isneg, 0);
+	if (isneg)
+		ret = ft_strjoin_free(ft_strdup("-"), ret);
+	return (ret);
 }
 
-void	intflag2(t_str *id, char *ret)
+char	*intflag22(t_str *id, char *ret, int *i)
+{
+	int	last;
+	int	isneg;
+
+	ret = getneg(ret, &isneg);
+	(*i)++;
+	last = *i;
+	while (ft_isdigit(id->str[*i]))
+		(*i)++;
+	last = ft_atoi_free(ft_substr(id->str, last, *i - last));
+	if (last > (int)ft_strlen(ret))
+		ret = addzero(ret, last - ft_strlen(ret), 0);
+	else if (ft_strlen(ret) == 1 && ret[0] == '0' && last < 1)
+	{
+		free(ret);
+		ret = ft_strdup("");
+	}
+	if (isneg)
+		ret = ft_strjoin_free(ft_strdup("-"), ret);
+	return (ret);
+}
+
+char	*intflag2(t_str *id, char *ret)
 {
 	int	i;
-	int	isneg;
-	int	last;
 
 	i = 1;
 	while (id->str[i])
 	{
 		if (i == 1 && id->str[i] == '0' && !aschr(id->str, '.'))
-		{
-			ret = getneg(ret, &isneg);
-			while (id->str[i] == '0')
-				i++;
-			last = i;
-			while (ft_isdigit(id->str[i]))
-				i++;
-			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last > (int)ft_strlen(ret))
-				ret = addzero(ret, last - ft_strlen(ret) - isneg, 0);
-			if (isneg)
-				ret = ft_strjoin_free(ft_strdup("-"), ret);
-		}
-		intflag21(id, ret, &i, &last, &isneg);
+			ret = intflag21(id, ret, &i);
+		if (id->str[i] == '.')
+			ret = intflag22(id, ret, &i);
 		i++;
 	}
+	return (ret);
 }
 
-void	intflag31(t_str *id, char *ret, int *i, int *last, int *isneg)
-{
-	if (id->str[*i] == '-')
-	{
-		while (id->str[*i] == '-')
-			*i++;
-		*last = *i;
-		while (ft_isdigit(id->str[*i]))
-			*i++;
-		*last = ft_atoi_free(ft_substr(id->str, *last, *i - *last));
-		if (*last < (int)ft_strlen(ret))
-		{
-			free(id->str);
-			id->str = ret;
-			return ;
-		}
-		ret = addspace(ret, (*last - ft_strlen(ret)), 1);
-	}
-}
-
-void	intflag3(t_str *id, char *ret)
+int	intflag3(t_str *id, char **ret)
 {
 	int	i;
 	int	last;
-	int	isneg;
 
 	i = 1;
 	while (id->str[i])
@@ -136,27 +123,42 @@ void	intflag3(t_str *id, char *ret)
 			while (ft_isdigit(id->str[i]))
 				i++;
 			last = ft_atoi_free(ft_substr(id->str, last, i - last));
-			if (last < (int)ft_strlen(ret))
+			if (last < (int)ft_strlen(*ret))
 			{
 				free(id->str);
-				id->str = ret;
-				return ;
+				id->str = *ret;
+				return (1);
 			}
-			ret = addspace(ret, (last - ft_strlen(ret)), 0);
+			*ret = addspace(*ret, (last - ft_strlen(*ret)), 0);
 		}
-		intflag31(id, ret, &i, &last, &isneg);
+		if (id->str[i] == '-')
+		{
+			while (id->str[i] == '-')
+				i++;
+			last = i;
+			while (ft_isdigit(id->str[i]))
+				i++;
+			last = ft_atoi_free(ft_substr(id->str, last, i - last));
+			if (last < (int)ft_strlen(*ret))
+			{
+				free(id->str);
+				id->str = *ret;
+				return (1);
+			}
+			*ret = addspace(*ret, (last - ft_strlen(*ret)), 1);
+		}
 		i++;
 	}
+	return (0);
 }
 
 void	intflag(t_str *id, char *ret)
 {
-	int	i;
-	int	last;
 	int	isneg;
 
-	intflag2(id, ret);
-	intflag3(id, ret);
+	ret = intflag2(id, ret);
+	if (intflag3(id, &ret))
+		return ;
 	if (aschr(id->str, ' '))
 	{
 		ret = getneg(ret, &isneg);
